937ccc053817c534a7aeacba41b09cd16b15ff74
thush <33790334+krthush@users.noreply.github.com>
2021-02-11 13:47:37 +0000

    using tasklist to check server state

--- src/bot.js
+++ src/bot.js
@@ -4,9 +4,9 @@ var exec = require('child_process').execFile;
 
 // constants
 const { Client } = require('discord.js'); // discord api
-const chokidar = require('chokidar'); // watching library for file changes
 const fs = require('fs');
 const readline = require('readline');
+const tasklist = require('tasklist');
 Tail = require('tail').Tail;
 const client = new Client();
 const PREFIX = process.env.COMMAND_PREFIX;
@@ -14,20 +14,20 @@ const ADMIN_ROLE = process.env.ADMIN_ROLE;
 const MAIN_CHANNEL_ID = process.env.MAIN_CHANNEL_ID;
 const AHK_SCRIPT = 'DSTServerInput.exe';
 const STARTUP_SCRIPT = 'StartDSTServer.bat';
-
+const DST_SERVER_TASK_NAME = 'dontstarve_dedicated_server_nullrenderer.exe'
 
 // vars
-var serverBooting = false;
-var serverRunning = false;
+var serverStartup = false;
+var serverShutdown = false;
 
 client.on('ready', () => {
 
     console.log(`${client.user.username} has logged in.`);
     const mainChannel = client.channels.cache.find(channel => channel.id === MAIN_CHANNEL_ID);
-    // mainChannel.send("I'm awake and ready to not starve!");
+    mainChannel.send("I'm awake and ready to not starve!");
 
     tail = new Tail("C:/Users/krthu/Documents/Klei/DoNotStarveTogether/MyDediServer/Master/server_log.txt");
-    
+
     tail.on("line", function(data) {
         console.log(data);
     });
@@ -37,21 +37,8 @@ client.on('ready', () => {
     //     crlfDelay: Infinity
     // });
 
-    // check if log file changed
-    chokidar.watch("C:/Users/krthu/Documents/Klei/DoNotStarveTogether/MyDediServer/Master/server_log.txt", {
-        usePolling: true
-    }).on('all', (event, path) => {
-        console.log(event, path);
-    });
-
-    // check if log file changed
-    // fs.watchFile("C:/Users/krthu/Documents/Klei/DoNotStarveTogether/MyDediServer/Master/server_log.txt", (curr, prev) => {
-    //     console.log("file change");
-    // });
-
-    // check if log file changed
-    // fs.watch("C:/Users/krthu/Documents/Klei/DoNotStarveTogether/MyDediServer/Master/server_log.txt", (event, filename) => {
-    //     console.log(event, filename);
+    // rl.on('line', (line) => {
+    //     console.log(`Line from file: ${line}`);
     // });
 
 });
@@ -74,61 +61,71 @@ client.on('message', (message) => {
 
             // startup command
             if (command === "startup") {
-                if (!serverRunning) {
-                    if (!serverBooting) {
-                        serverBooting = true;
-                        console.log("Starting up DST server...");
-                        message.channel.send("Starting up DST server...");
-                        // use child process to run .bat
-                        exec(STARTUP_SCRIPT, function(err, data) {  
-                            console.log(err)
-                            console.log(data.toString());                       
-                        });
-                        serverRunning = true;
-                        serverBooting = false;
+                isDSTServerOnline().then((serverOnline) => {
+                    if (!serverOnline) {
+                        if (!serverStartup) {
+                            serverStartup = true;
+                            console.log("Starting up DST server...");
+                            message.channel.send("Starting up DST server...");
+                            // use child process to run .bat
+                            exec(STARTUP_SCRIPT, function(err, data) {  
+                                console.log(err)
+                                console.log(data.toString());                       
+                            });
+                            serverStartup = false; // MOVE
+                        } else {
+                            message.channel.send("Attempting server startup already.");
+                        }
                     } else {
-                        message.channel.send("Attempting server startup already.");
+                        message.channel.send("Server already online.");
                     }
-                } else {
-                    message.channel.send("Server already running.");
                 }
             }
 
             // shutdown command
             if (command === "shutdown") {
-                if (serverRunning) {
-                    console.log("Shutting down DST server...");
-                    message.channel.send("Shutting down DST server...");
-                    // use child process to run .exe
-                    runDSTServerCommand("caves", "c_shutdown()");
-                    // shut master down after small delay
-                    setTimeout(function(){ runDSTServerCommand("master", "c_shutdown()"); }, 1000);
-                    serverRunning = false;
-                } else {
-                    message.channel.send("Attempting server shutdown already.");
-                }
+                isDSTServerOnline().then((serverOnline) => {
+                    if (serverOnline) {
+                        if (!serverShutdown) {
+                            serverShutdown = true;
+                            console.log("Shutting down DST server...");
+                            message.channel.send("Shutting down DST server...");
+                            // use child process to run .exe
+                            runDSTServerCommand("caves", "c_shutdown()");
+                            // shut master down after small delay
+                            setTimeout(function(){ runDSTServerCommand("master", "c_shutdown()"); }, 1000);
+                            serverShutdown = false; // MOVE
+                        } else {
+                            message.channel.send("Attempting server shutdown already.");
+                        }
+                    } else {
+                        message.channel.send("Server already offline.");
+                    }
+                });
             }
 
             // general command
             if (command === "general") {
-                if (serverRunning) {
-                    console.log("Running general command.");
-                    message.channel.send("Running general command. I hope you know what you're doing.");
-                    if (args[0]=="master") {
-                        args.shift();
-                        // clean string for cmd line parameters
-                        const dstCMD = args.join(' ').replace(/["]{2,}/g,'');
-                        runDSTServerCommand("master", dstCMD);
-                    } else if (args[0]=="caves") {
-                        args.shift();
-                        // clean string for cmd line parameters
-                        const dstCMD = args.join(' ').replace(/["]{2,}/g,'');
-                        runDSTServerCommand("caves", dstCMD);
+                isDSTServerOnline().then((serverOnline) => {
+                    if (serverOnline) {
+                        console.log("Running general command.");
+                        message.channel.send("Running general command. I hope you know what you're doing.");
+                        if (args[0]=="master") {
+                            args.shift();
+                            // clean string for cmd line parameters
+                            const dstCMD = args.join(' ').replace(/["]{2,}/g,'');
+                            runDSTServerCommand("master", dstCMD);
+                        } else if (args[0]=="caves") {
+                            args.shift();
+                            // clean string for cmd line parameters
+                            const dstCMD = args.join(' ').replace(/["]{2,}/g,'');
+                            runDSTServerCommand("caves", dstCMD);
+                        } else {
+                            message.channel.send("Please specific shard (master/caves) in first parameter");
+                        }
                     } else {
-                        message.channel.send("Please specific shard (master/caves) in first parameter");
+                        message.channel.send("The server is shutdown.");
                     }
-                } else {
-                    message.channel.send("Server offline.");
                 }
             }
 
@@ -145,4 +142,16 @@ function runDSTServerCommand(shard, command) {
             console.log("Error:" + err);
         }                      
     });
+}
+
+async function isDSTServerOnline() {
+    const filterString = `Imagename eq ${DST_SERVER_TASK_NAME}`;
+    const tasks = await tasklist({
+        filter: [filterString]
+    });
+    if (tasks.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
